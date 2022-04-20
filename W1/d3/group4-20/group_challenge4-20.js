1f157c683bac059632a32f67895d524d34c0fb52
Chuck Breter <chuckb0384@aim.com>
2022-04-20 15:00:59 -0400

    registration form

---
+++ W1/d3/group4-20/group_challenge4-20.js
@@ -0,0 +1,11 @@
+
+
+    var countpositives = 0;
+    var numbers = [3, 4, -2, 7, 16, -8, 0];
+    
+    for(var i = 0; i < numbers.length; i++) {
+        if(numbers[i] > 0) {
+            countpositives++
+        }
+    }
+    console.log(countpositives)
