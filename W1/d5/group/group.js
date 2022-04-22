67e9084fa2ca38a4778338662ce52ff8a3cd6e4e
Chuck Breter <chuckb0384@aim.com>
2022-04-22 13:54:10 -0400

    plotting_blocks

---
+++ W1/d5/group/group.js
@@ -0,0 +1,12 @@
+
+var flippedarray[];
+
+function flip(array)
+    for(var i = 0; i < Array.length; i++) {
+        var flippedarray = array.length-1-i;
+        console.log(flippedarray);
+    }
+
+
+flip[4, 3, 2, 9]
+console.log(flippedarray)
