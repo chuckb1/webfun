f37d6b48ab0581e6b6963933113cf3f098db9313
Chuck Breter <chuckb0384@aim.com>
2022-04-25 15:36:40 -0400

    navBar

---
+++ w2/d1/group/index.js
@@ -0,0 +1,12 @@
+function roll1to6() {
+    for(var i=1; i < 10; i++) {
+        var roll = Math.floor(Math.random() * 6 + 1);
+        console.log(roll)
+    }
+}
+
+function life() {
+    var roll = Math.floor(Math.random() * 20 + 1);
+        return life  
+    }
+}
