8673c36b81817b36b6ba56538deb5f6c0eeffe94
Chuck Breter <chuckb0384@aim.com>
2022-04-28 12:53:10 -0400

    likes

---
+++ w2/d2/likes/script.js
@@ -0,0 +1,24 @@
+var x = 0;
+var countElement = document.querySelector("#count");
+
+function countLikes() {
+    x++;
+    countElement.innerText = x + " Likes";
+    console.log(x);
+}
+var y = 0;
+var countElement1 = document.querySelector("#count1");
+
+function countLikes1() {
+    y++;
+    countElement1.innerText = y + " Likes";
+    console.log(y);
+}
+var z = 0;
+var countElement2 = document.querySelector("#count2");
+
+function countLikes2() {
+    z++;
+    countElement2.innerText = z + " Likes";
+    console.log(z);
+}
